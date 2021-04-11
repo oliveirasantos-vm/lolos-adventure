6f3d2453c645b1485084351666d61ea69850c0d4
Vitor Santos <oliveirasantos.vm@gmail.com>
2021-04-10 21:40:28 -0300

    Import raylib librarie and create first window.

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <raylib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+#define MAX_FONTS   8
+
+int main(){
+    const int WindowWidth = 640;
+    const int WindowHeight = 480;
+
+    InitWindow(WindowWidth, WindowHeight, "Lolo's Adventure");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
     return 0;
 }
